4f4249a8b48de5714374f69f27be39e164aa9687
DinishibaS <dinishiba@gmail.com>
2021-05-25 21:36:15 +0530

    final update

--- main.js
+++ main.js
@@ -76,12 +76,14 @@ sr.reveal('#timeline-header-5', {origin:'bottom', delay: 300})
 sr.reveal('#timeline-content-5', {origin:'bottom', delay: 300})
 sr.reveal('#timeline-header-6', {origin:'bottom', delay: 300})
 sr.reveal('#timeline-content-6', {origin:'bottom', delay: 300})
-sr.reveal('.contact__input', {delay: 400})
+
 sr.reveal('.contact__button', {delay: 600})
 
 
 
 sr.reveal('.endline', {delay: 500, interval: 100})
 
+sr.reveal('.aboutimg', {origin:'bottom', delay: 300})
 
-
+sr.reveal('.aboutcontent', {origin:'bottom', delay: 500})
+sr.reveal('.aboutcontent2', {origin:'bottom', delay: 600})
